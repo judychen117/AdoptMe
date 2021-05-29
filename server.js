30ce4658b7032fb23d87b278a840856af4a861cc
judy <judychen117@gmail.com>
2021-05-29 15:45:04 +0800

    third commit

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
-const express = require('express')
-const serveStatic = require('serve-static')
-const path = require('path')
+const express = require('express');
+const { publicPath } = require('./vue.config');
+const port = process.env.PORT || 8080;
 
-const app = express()
+// const serveStatic = require('serve-static')
+// const path = require('path')
 
-app.use('/', serveStatic(path.join(__dirname, '/dist')))
+const app = express();
 
-const port = process.env.PORT || 8080
-app.listen(port)
+//app.use('/', serveStatic(path.join(__dirname, '/dist')))
+
+app.use(express.static(__dirname + "/dist/"));
+app.get(/.*/, function (req, res) {
+    res.sendFile(__dirname + "dist/index.html")
+})
+app.listen(port);
 
 console.log(`listening on port 8080`)
